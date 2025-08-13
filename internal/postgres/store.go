41f7e2180d02573c78cf0327fad73b5b702845d0
gordon <gordonchen2014@gmail.com>
2025-08-12 23:43:47 -0400

    feat: new store interface

---
+++ internal/postgres/store.go
@@ -0,0 +1,86 @@
+package postgres
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rank1zen/kevin/internal"
+	"github.com/rank1zen/kevin/internal/riot"
+)
+
+// TODO: replace Store.
+type Store2 struct {
+	Pool *pgxpool.Pool
+}
+
+func NewStore2() internal.Store2 {
+	return &Store2{}
+}
+
+func (db *Store2) RecordProfile(ctx context.Context, summoner internal.Profile) error {
+	tx, err := db.Pool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer tx.Rollback(ctx)
+
+	rank := RankStore{Tx: tx}
+
+	// create or update summoner
+
+	rankStatus := RankStatus{
+		PUUID:         summoner.PUUID.String(),
+		EffectiveDate: summoner.Rank.EffectiveDate,
+		IsRanked:      false,
+	}
+
+	if summoner.Rank.Detail != nil {
+		rankStatus.IsRanked = true
+	}
+
+	statusID, err := rank.CreateRankStatus(ctx, rankStatus)
+	if err != nil {
+		return err
+	}
+
+	if summoner.Rank.Detail != nil {
+		rankDetail := RankDetail{
+			RankStatusID: statusID,
+			Wins:         summoner.Rank.Detail.Wins,
+			Losses:       summoner.Rank.Detail.Wins,
+			Tier:         summoner.Rank.Detail.Rank.Tier.String(),
+			Division:     summoner.Rank.Detail.Rank.Division.String(),
+			LP:           summoner.Rank.Detail.Rank.LP,
+		}
+
+		if err := rank.CreateRankDetail(ctx, rankDetail); err != nil {
+			return err
+		}
+	}
+
+	tx.Commit(ctx)
+
+	return nil
+}
+
+func (db *Store2) GetProfileDetail(ctx context.Context, puuid riot.PUUID) (internal.ProfileDetail, error) {
+	panic("not implemented")
+}
+
+func (db *Store2) RecordMatch(ctx context.Context, match internal.Match) error {
+	panic("not implemented")
+}
+
+func (db *Store2) GetMatchDetail(ctx context.Context, id riot.PUUID) (internal.MatchDetail, error) {
+	panic("not implemented")
+}
+
+func (db *Store2) GetMatchHistory(ctx context.Context, puuid riot.PUUID, start, end time.Time) ([]internal.SummonerMatch, error) {
+	panic("not implemented")
+}
+
+func (db *Store2) GetNewMatchIDs(ctx context.Context, ids []string) (newIDs []string, err error) {
+	panic("not implemented")
+}
