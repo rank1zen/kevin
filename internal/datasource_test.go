2942259944d8f30812bd43a0e3c5613bfbf9bc87
Shen Chen <gordonchen2014@gmail.com>
2025-08-19 01:21:04 -0400

    test: add happy path datasource tests

--- internal/datasource_test.go
+++ internal/datasource_test.go
@@ -2,20 +2,69 @@ package internal_test
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/rank1zen/kevin/internal"
+	"github.com/rank1zen/kevin/internal/postgres"
 	"github.com/rank1zen/kevin/internal/riot"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDatasource_GetMatchDetail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
 	ctx := context.Background()
 
-	riotClient := riot.NewClient("")
+	riotClient := riot.NewClient(getEnvRiotAPIKey())
 
-	datasource := internal.NewDatasource(riotClient, nil)
+	pgPool := DefaultPGInstance.SetupConn(ctx, t)
+	store := postgres.NewStore(pgPool)
 
-	match, err := datasource.GetMatchDetail(ctx, riot.RegionNA1, "m")
-	require.NoError(t, err)
+	datasource := internal.NewDatasource(riotClient, store)
+
+	match, err := datasource.GetMatchDetail(ctx, riot.RegionNA1, "NA1_5346312088")
+	if assert.NoError(t, err) {
+		for _, tc := range []struct {
+			Name             string
+			Expected, Actual any
+		}{
+			{
+				Name:     "expects correct version",
+				Expected: "15.16.702.7993",
+				Actual:   match.Version,
+			},
+			{
+				Name:     "expects correct participant gold percentage",
+				Expected: float32(6856.0 / 41017),
+				Actual:   match.Participants[0].SummonerIDs,
+			},
+		} {
+			t.Run(tc.Name, func(t *testing.T) { assert.Equal(t, tc.Expected, tc.Actual) })
+		}
+	}
+}
+
+func TestDatasource_GetProfileDetail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	ctx := context.Background()
+
+	riotClient := riot.NewClient(getEnvRiotAPIKey())
+
+	pgPool := DefaultPGInstance.SetupConn(ctx, t)
+	store := postgres.NewStore(pgPool)
+
+	datasource := internal.NewDatasource(riotClient, store)
+
+	_, err := datasource.GetProfileDetail(ctx, riot.RegionNA1, T1OKGOODYESNA1PUUID)
+	assert.NoError(t, err)
+}
+
+func getEnvRiotAPIKey() string {
+	return os.Getenv("KEVIN_RIOT_API_KEY")
 }
--- internal/internal_test.go
+++ internal/internal_test.go
@@ -1,5 +1,29 @@
 package internal_test
 
-import "github.com/rank1zen/kevin/internal"
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/rank1zen/kevin/internal"
+	"github.com/rank1zen/kevin/internal/postgres"
+)
+
+var DefaultPGInstance *postgres.PGInstance
+
+func TestMain(t *testing.M) {
+	ctx := context.Background()
+
+	DefaultPGInstance = postgres.NewPGInstance(context.Background())
+
+	code := t.Run()
+
+	if err := DefaultPGInstance.Terminate(ctx); err != nil {
+		log.Fatalf("terminating: %s", err)
+	}
+
+	os.Exit(code)
+}
 
 var T1OKGOODYESNA1PUUID = internal.NewPUUIDFromString("44Js96gJP_XRb3GpJwHBbZjGZmW49Asc3_KehdtVKKTrq3MP8KZdeIn_27MRek9FkTD-M4_n81LNqg")
