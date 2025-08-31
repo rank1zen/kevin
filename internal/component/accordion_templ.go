bc9540eb349a77155fbe6ca40072bae9d83e1b76
Shen Chen <gordonchen2014@gmail.com>
2025-08-31 01:40:43 -0400

    style: accordion

--- internal/component/accordion_templ.go
+++ internal/component/accordion_templ.go
@@ -8,9 +8,7 @@ package component
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-import (
-	"context"
-)
+import "context"
 
 type Accordion struct {
 	Children Component
@@ -39,7 +37,7 @@ func (m Accordion) ToTempl(ctx context.Context) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div x-id=\"['accordion']\" x-data=\"{ open: false }\"><div class=\"bg-white border border-gray-900/10 rounded-2xl dark:bg-black dark:border-gray-100/10\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div role=\"region\" x-id=\"['accordion']\" x-data=\"{ open: false }\"><div class=\"bg-white border border-gray-200 rounded-2xl dark:bg-black dark:border-neutral-600\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -49,7 +47,7 @@ func (m Accordion) ToTempl(ctx context.Context) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div><div class=\"bg-gray-50 border border-gray-900/10 rounded-2xl mt-2 shadow-xs shadow-gray-900/10 dark:bg-gray-950 dark:border-gray-100/10 dark:shadow-gray-100/10\" x-show=\"open\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div><div x-show=\"open\" x-cloak class=\"bg-gray-50 border border-gray-200 rounded-2xl mt-2 dark:bg-neutral-900 dark:border-neutral-600\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -67,6 +65,7 @@ func (m Accordion) ToTempl(ctx context.Context) templ.Component {
 	})
 }
 
+// LazyAccordion lazy loads data into the expanded section.
 type LazyAccordion struct {
 	Children Component
 
@@ -98,7 +97,7 @@ func (m LazyAccordion) ToTempl(ctx context.Context) templ.Component {
 			templ_7745c5c3_Var2 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "<div x-id=\"['accordion']\" x-data=\"{ open: false }\"><div class=\"bg-white border border-gray-900/10 rounded-2xl dark:bg-black dark:border-gray-100/10\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "<div role=\"region\" x-id=\"['accordion']\" x-data=\"{ open: false }\"><div class=\"bg-white border border-gray-200 rounded-2xl dark:bg-black dark:border-neutral-600\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -115,7 +114,7 @@ func (m LazyAccordion) ToTempl(ctx context.Context) templ.Component {
 		var templ_7745c5c3_Var3 string
 		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(m.Path)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/component/accordion.templ`, Line: 57, Col: 19}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/component/accordion.templ`, Line: 61, Col: 19}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 		if templ_7745c5c3_Err != nil {
@@ -133,7 +132,7 @@ func (m LazyAccordion) ToTempl(ctx context.Context) templ.Component {
 			var templ_7745c5c3_Var4 string
 			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(string(m.Data))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/component/accordion.templ`, Line: 61, Col: 28}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/component/accordion.templ`, Line: 65, Col: 28}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 			if templ_7745c5c3_Err != nil {
@@ -144,7 +143,7 @@ func (m LazyAccordion) ToTempl(ctx context.Context) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, " x-show=\"open\" class=\"bg-gray-50 border border-gray-900/10 rounded-2xl mt-2 shadow-xs shadow-gray-900/10 dark:bg-gray-950 dark:border-gray-100/10 dark:shadow-gray-100/10\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, " x-show=\"open\" x-cloak class=\"bg-gray-50 border border-gray-200 rounded-2xl mt-2 dark:bg-neutral-900 dark:border-neutral-600\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -235,7 +234,7 @@ func (m AccordionTrigger) ToTempl(ctx context.Context) templ.Component {
 			templ_7745c5c3_Var6 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 15, "<button type=\"button\" @click=\"\n\t\t\topen = !open;\n\t\t\t$dispatch('visible');\n\t\t\" class=\"flex items-center justify-center size-9 rounded-lg transition cursor-pointer dark:hover:bg-gray-100/5 dark:active:bg-gray-100/10 hover:bg-gray-900/5 focus-visible:inset-ring-blue-500/50 focus-visible:inset-ring-2 focus-visible:outline-0 active:bg-gray-900/10\" :class=\"open && 'bg-gray-900/5 dark:bg-gray-900/5'\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 15, "<button type=\"button\" @click=\"\n\t\t\topen = !open;\n\t\t\t$dispatch('visible');\n\t\t\" :aria-expanded=\"open\" class=\"flex items-center justify-center size-9 rounded-lg transition cursor-pointer dark:hover:bg-neutral-100/20 dark:active:bg-neutral-100/40 hover:bg-gray-900/5 focus-visible:inset-ring-blue-500/50 focus-visible:inset-ring-2 focus-visible:outline-0 active:bg-gray-900/10\" :class=\"open && 'bg-gray-900/5 dark:bg-neutral-100/20'\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
