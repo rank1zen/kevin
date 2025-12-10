1d5cc1695e60a05a4a7efb589d00fde01fadd252
Shen Chen <gordonchen2014@gmail.com>
2025-12-09 23:29:23 -0500

    feat: search methods and tests

--- internal/service/search.go
+++ internal/service/search.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"strings"
 
 	"github.com/rank1zen/kevin/internal"
 	"github.com/rank1zen/kevin/internal/riot"
@@ -14,11 +15,62 @@ type SearchProfileRequest struct {
 	Query  string       `json:"query"`
 }
 
-func (s *SearchService) SearchProfile(ctx context.Context, req SearchProfileRequest) ([]internal.SearchResult, error) {
+func (s *SearchService) SearchProfile(ctx context.Context, req SearchProfileRequest) (*SearchResult, error) {
+	if req.Region == nil {
+		req.Region = new(riot.Region)
+		*req.Region = riot.RegionNA1
+	}
+
+	var name, tag string
+
+	if index := strings.Index(req.Query, "#"); index != -1 {
+		name = req.Query[:index]
+		tag = req.Query[index+1:]
+	} else {
+		name = req.Query
+	}
+
 	storeResults, err := s.store.Profile.SearchSummoner(ctx, req.Query)
 	if err != nil {
 		return nil, err
 	}
 
-	return storeResults, nil
+	result := SearchResult{
+		Region:   *req.Region,
+		Name:     name,
+		Tag:      tag,
+		Profiles: []ProfileSearchResult{},
+	}
+
+	for _, storeResult := range storeResults {
+		result.Profiles = append(result.Profiles, ProfileSearchResult{
+			Region: *req.Region,
+			Name:   name,
+			Tag:    tag,
+			PUUID:  storeResult.PUUID,
+			Rank:   nil, // TODO: Implement rank fetching logic
+		})
+	}
+
+	return &result, nil
+}
+
+type SearchResult struct {
+	Region riot.Region
+
+	// Name and Tag is the name#tag that is used for search.
+	Name, Tag string
+
+	Profiles []ProfileSearchResult
+}
+
+type ProfileSearchResult struct {
+	Region riot.Region
+
+	PUUID riot.PUUID
+
+	Name, Tag string
+
+	// Rank is the summoners most recent rank record in store.
+	Rank *internal.RankStatus
 }
