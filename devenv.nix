a1903ed63d29e6cc357775298bf2577adb3465b7
Shen <85842176+rank1zen@users.noreply.github.com>
2025-12-10 23:48:06 -0500

    chore: switch to devenv for managing environment (#29)

---
+++ devenv.nix
@@ -0,0 +1,16 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  env = {
+    TERN_MIGRATIONS = "./migrations";
+  };
+
+  packages = [
+    pkgs.git
+    pkgs.golangci-lint
+    pkgs.tailwindcss_4
+    pkgs.rustywind
+  ];
+
+  languages.go.enable = true;
+}
