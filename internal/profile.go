f434e47f5155b8cf9a39b4530ba242dab3f378e7
Shen Chen <gordonchen2014@gmail.com>
2025-12-17 17:13:29 -0500

    add some docs

--- internal/profile.go
+++ internal/profile.go
@@ -18,11 +18,15 @@ type ProfileStore interface {
 	SearchByNameTag(ctx context.Context, name, tag string) ([]Profile, error)
 }
 
+// Profile is a summoner's profile. PUUID is unique and immutable. Name +
+// Tagline is unique but mutable.
 type Profile struct {
 	PUUID riot.PUUID
 
 	Name, Tagline string
 
+	// Rank is the summoners most recent rank record in store, meaning it could be
+	// out-of-date.
 	Rank RankStatus
 }
 
