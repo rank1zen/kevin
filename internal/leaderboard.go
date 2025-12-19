ff5fb8f09c98ba335523ab378cf0557e0322315c
Shen Chen <gordonchen2014@gmail.com>
2025-12-19 10:50:14 -0500

    feat: LeaderboardStore interface

---
+++ internal/leaderboard.go
@@ -0,0 +1,41 @@
+package internal
+
+import (
+	"context"
+
+	"github.com/rank1zen/kevin/internal/riot"
+)
+
+// LeaderboardStore provides access to leaderboard data.
+type LeaderboardStore interface {
+	GetLeaderboard(ctx context.Context, region riot.Region, filter LeaderboardFilter) (*Leaderboard, error)
+}
+
+// Leaderboard is the top 500 players in a region
+type Leaderboard struct {
+	Region string
+
+	Entries []LeaderboardEntry
+}
+
+// LeaderboardEntry is an entry for a player in a leaderboard.
+type LeaderboardEntry struct {
+	Region string
+
+	PUUID string
+
+	Name, Tag string
+
+	Tier string
+
+	Division string
+
+	LP int
+
+	Wins, Losses int
+}
+
+type LeaderboardFilter struct {
+	Start int
+	Count int
+}
