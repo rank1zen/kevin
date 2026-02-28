2fc4cf42be14f746591248ce7981881767205271
Shen Chen <gordonchen2014@gmail.com>
2026-02-28 01:48:03 -0500

    feat: create not_found_page

---
+++ internal/web/page/not_found_page/handler.go
@@ -0,0 +1,24 @@
+package not_found_page
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/rank1zen/kevin/internal/frontend"
+)
+
+type Handler struct{}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c := Index(r.Context(), &IndexData{})
+
+	if err := c.Render(r.Context(), w); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		frontend.LogError(r, errors.New("templ error"))
+		return
+	}
+}
