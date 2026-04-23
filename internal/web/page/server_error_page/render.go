6a4d12e43ef90b49e3fcc83b164c44a8a21af800
Gordon <85842176+rank1zen@users.noreply.github.com>
2026-04-23 12:38:50 -0400

    refactor!: remove frontend package (#67)

--- internal/web/page/server_error_page/render.go
+++ internal/web/page/server_error_page/render.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/rank1zen/kevin/internal/frontend"
+	"github.com/rank1zen/kevin/internal/web"
 )
 
 func Render(w http.ResponseWriter, r *http.Request, err error, msg string) {
-	frontend.LogError(r, fmt.Errorf("%s: %w", msg, err))
+	web.LogError(r, fmt.Errorf("%s: %w", msg, err))
 	w.WriteHeader(http.StatusInternalServerError)
 	_ = Index(r.Context(), &IndexData{}).Render(r.Context(), w)
 }
