099c340cd8c6f5bb5c5b2d3822bcac9b14a30e6c
Gordon Chen <gordonchen2014@gmail.com>
2026-08-18 20:47:41 -0400

    chore!: baseline the migrations

--- migrations/003_rankstatus_drop_cols.sql
+++
@@ -1,8 +0,0 @@
--- Write your migrate up statements here
-
-alter table RankStatus
-        drop column end_date,
-        drop column is_current;
-
--- Write your migrate down statements here. If this migration is irreversible
--- Then delete the separator line above.
